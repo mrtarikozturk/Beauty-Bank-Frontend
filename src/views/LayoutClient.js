7dcf8ef6b5ca2801286e9740020c822c3dfd4511
Mr. Tarik OZTURK <mr.tarik.ozturk@gmail.com>
2021-04-04 14:05:59 +0200

    Updated theme as secondary

--- src/views/LayoutClient.js
+++ src/views/LayoutClient.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import { useHistory } from 'react-router';
 
-import {Dashboard as DashboardIon, AccountCircle, PostAdd as PostAddIcon} from '@material-ui/icons'
+import { Dashboard as DashboardIon, AccountCircle, PostAdd as PostAddIcon } from '@material-ui/icons'
 
 import Layout from '../components/Layout'
 
-export const LayoutClient = ({children, pageTitle}) => {
+export const LayoutClient = ({ children, pageTitle }) => {
   const history = useHistory()
 
   const list = [
     {
-      icon: <AccountCircle color='primary' />,
+      icon: <AccountCircle color='secondary' />,
       title: "Profile",
       onClick: () => history.push('/client-profile')
     },
     {
-      icon: <DashboardIon color='primary' />,
+      icon: <DashboardIon color='secondary' />,
       title: "Dashboard",
       onClick: () => history.push('/client')
     },
     {
-      icon: <PostAddIcon color='primary' />,
-      title: "Create Ticket",
+      icon: <PostAddIcon color='secondary' />,
+      title: "Make Request",
       onClick: () => history.push('/create-ticket')
     }
   ]
