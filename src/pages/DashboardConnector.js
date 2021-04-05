7860dab5c047975fe5d5d3758afcfc768cb4bcc4
Mr. Tarik OZTURK <mr.tarik.ozturk@gmail.com>
2021-04-05 12:34:44 +0200

    Added intake call and intake call date colums

--- src/pages/DashboardConnector.js
+++ src/pages/DashboardConnector.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from "react";
-import { useSnackbar } from "notistack";
 
-import { makeStyles, Button } from "@material-ui/core";
-
+import { makeStyles, Button, TextField, Avatar, IconButton } from "@material-ui/core";
+import { DateTimePicker } from "@material-ui/pickers";
+import AutorenewIcon from '@material-ui/icons/Autorenew';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 import { Dashboard } from "../components/Dashboard";
 import { LayoutConnector } from "../views";
 import { FormatDate, FormatDateTime } from "../helper/FormatDate";
 import { AssignPro } from "../components/AssignPro";
+import api, { handleError } from "../api";
+import { useSnackbar } from "notistack";
+import AddIcCallIcon from '@material-ui/icons/AddIcCall';
 
-import api from "../api";
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -20,6 +23,9 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     flexDirection: "column",
     marginBottom: theme.spacing(10),
+  },
+  datePicker: {
+    width: '135px',
   },
   fixedHeight: {
     height: 240,
@@ -40,7 +46,7 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: "column",
   },
   button: {
-    width: 150,
+    width: 'auto',
     fontSize: 12,
     paddingLeft: 2,
     paddingRight: 2,
@@ -49,19 +55,24 @@ const useStyles = makeStyles((theme) => ({
 
 export const DashboardConnector = () => {
   const classes = useStyles();
-  const { enqueueSnackbar } = useSnackbar();
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+
 
   const [open, setOpen] = useState(false);
   const [selectedTicket, setSelectedTicket] = useState([]);
   const [loading, setLoading] = useState(true);
   const [tickets, setTickets] = useState([]);
+  // const [date, handleDateChange] = useState(new Date("2018-01-01T00:00:00.000Z"));
+  const [date, setDate] = useState(new Date());
+  const [modalName, setModalName] = useState('');
 
   const [page, setPage] = useState(1);
   const [pageSize, setPageSize] = useState(null);
 
-  const handleOpen = (ticket) => {
+  const handleOpen = (ticket, name) => {
     setOpen(true);
     setSelectedTicket(ticket);
+    setModalName(name);
   };
 
   const handleClose = () => {
@@ -80,8 +91,23 @@ export const DashboardConnector = () => {
       .finally(() => setLoading(false));
   }, [open, page]);
 
-  const dateParams = { selectedTicket, handleClose };
+  const dateParams = { selectedTicket, handleClose, modalName };
   const modalParams = { open, onModalClose: handleClose };
+
+  async function handleDateChange(ticket) {
+    alert(date)
+    api
+      .put(`/ticket/connector-intake/${ticket.id}`, {
+        intake_call_date: date,
+      })
+      .then(() => {
+        enqueueSnackbar("Successfull!", {
+          variant: "success",
+        });
+      })
+      .catch(handleError(enqueueSnackbar, closeSnackbar));
+  }
+
 
   return (
     <Dashboard
@@ -92,7 +118,7 @@ export const DashboardConnector = () => {
       pagination={{ pageSize, setPage }}
       modals={[
         {
-          title: "Assign Pro",
+          title: modalName,
           content: <AssignPro {...dateParams} />,
           ...modalParams,
         },
@@ -105,6 +131,8 @@ export const DashboardConnector = () => {
           "Create Date",
           "Appointment Date",
           "Phone Number",
+          'Intake Call',
+          'Intake Call',
           "Assign Pro",
         ],
         body: [
@@ -115,8 +143,33 @@ export const DashboardConnector = () => {
             t?.appointment_date ? FormatDateTime(t.appointment_date) : "-",
           (t) => t.phone_number,
           (t) => (
+            !t?.intake_call_date ?
+              <Button
+                onClick={() => handleOpen(t, 'Intake Date')}
+                variant="outlined"
+                color={t?.intake_call_date ? "primary" : "secondary"}
+                value="intake"
+                className={classes.button}
+              >
+                {"Set Date"}
+              </Button>
+              :
+              <DateTimePicker
+                className={classes.datePicker}
+                value={t?.intake_call_date}
+                readOnly={t?.intake_call_date}
+                format="DD/MM/yyyy HH:mm"
+              />
+          ),
+          (t) => (
+            t?.is_intake_call ?
+              <CheckCircleIcon color='secondary' />
+              :
+              < AddIcCallIcon color='secondary' />
+          ),
+          (t) => (
             <Button
-              onClick={() => handleOpen(t)}
+              onClick={() => handleOpen(t, 'Assign Pro')}
               variant="outlined"
               color={t?.pro ? "primary" : "secondary"}
               value="Choose"
