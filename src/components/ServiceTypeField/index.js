4df984cda187ad329ffe8cdeb08ec95bc860c786
Mr. Tarik OZTURK <mr.tarik.ozturk@gmail.com>
2021-05-03 12:25:50 +0200

    Created ServiceTypeField component

--- src/components/ServiceTypeField/index.js
+++ src/components/ServiceTypeField/index.js
@@ -1,49 +1,46 @@
-import React from 'react'
-import { Checkbox, FormControl, Grid, Input, InputLabel, ListItemText, MenuItem, Select, makeStyles } from '@material-ui/core'
-import { useIntl } from 'react-intl';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+import { Checkbox, Input, ListItemText, MenuItem, } from '@material-ui/core'
+import SelectField from '../SelectField';
 
-const useStyles = makeStyles({
-    formControl: {
-        minWidth: '100%',
-        maxWidth: 275
-    },
-})
 
 const ServiceTypeField = (props) => {
-    const { services, selectedServices, handleChange, sm = 6 } = props;
-    const classes = useStyles();
-    const { formatMessage } = useIntl();
+    const { formik, sm } = props;
+    const [services, setServices] = useState([]);
+    const { REACT_APP_API_BASE_URL } = process.env;
+
+    useEffect(() => {
+        axios.get(`${REACT_APP_API_BASE_URL}auth/service-type/`)
+            .then(response => {
+                setServices(response?.data?.results);
+            })
+            .catch(err => {
+                console.log(err)
+            })
+            .then(() => console.log(services))
+    }, [])
 
     return (
-        <Grid item xs={12} sm={sm}>
-            <FormControl className={classes.formControl}>
-                <InputLabel id="mutiple-checkbox-label">
-                    {formatMessage({
-                        id: 'services',
-                        defaultMessage: 'Services'
-                    })}
-                </InputLabel>
-                <Select
-                    labelId="mutiple-checkbox-label"
-                    multiple
-                    required
-                    fullWidth
-                    value={selectedServices}
-                    onChange={handleChange}
-                    input={<Input />}
-                    renderValue={(selected) => selected.map(item => {
-                        return services.find(type => type.id == item)?.name
-                    }).join(', ')}
-                >
-                    {services && services?.map((item) => (
-                        <MenuItem key={item.id} value={item.id}>
-                            <Checkbox checked={selectedServices.indexOf(item.id) > -1} />
-                            <ListItemText primary={item.name} />
-                        </MenuItem>
-                    ))}
-                </Select>
-            </FormControl>
-        </Grid>
+        <SelectField
+            name='serviceType'
+            {...{ formik, sm }}
+            id='services'
+            defaultMessage='Services'
+            multiple
+            required
+            fullWidth
+            input={<Input />}
+            renderValue={(selected) => selected.map(item => {
+                return services.find(type => type.id == item)?.name
+            }).join(', ')}
+        >
+            {services && services?.map((item) => (
+                <MenuItem key={item.id} value={item.id}>
+                    <Checkbox checked={formik?.values?.serviceType?.indexOf(item.id) > -1} />
+                    <ListItemText primary={item.name} />
+                </MenuItem>
+            ))}
+        </SelectField>
     )
 }
 
